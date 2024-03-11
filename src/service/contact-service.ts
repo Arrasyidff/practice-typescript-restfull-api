6e33ba4af74639a312e937f7aeb4b42c85197f51
Arrasyid Fadel Fatonsyah <aff.anton20@gmail.com>
2024-03-11 08:53:08 +0800

    feat: create update contact api

--- src/service/contact-service.ts
+++ src/service/contact-service.ts
@@ -1,5 +1,5 @@
-import { User } from '@prisma/client'
-import { CreateContactRequest, ContactResponse, toContactResponse } from '../model/contact-model'
+import { Contact, User } from '@prisma/client'
+import { CreateContactRequest, ContactResponse, toContactResponse, UpdateContactRequest } from '../model/contact-model'
 import { ContactValidation } from '../validation/contact-validation'
 import { Validation } from '../validation/validation'
 import { prismaClient } from '../application/database'
@@ -17,10 +17,29 @@ export class ContactService {
         return toContactResponse(contact)
     }
 
+    static async isContactExists(username: string, contactId: number): Promise<Contact>
+    {
+        const contact = await prismaClient.contact.findUnique({where: {id: contactId, username: username}})
+        if (!contact) throw new ResponseError(404, 'Contact is not found')
+
+        return contact
+    }
+
     static async get(user: User, contactId: number): Promise<ContactResponse>
     {
-        const contact = await prismaClient.contact.findUnique({where: {id: contactId, username: user.username}})
-        if (!contact) throw new ResponseError(404, 'Contact is not found')
+        const contact = await this.isContactExists(user.username, contactId)
+        return toContactResponse(contact)
+    }
+
+    static async update(user: User, request: UpdateContactRequest): Promise<ContactResponse>
+    {
+        const updateRequest = Validation.validate(ContactValidation.UPDATE, request)
+        await this.isContactExists(user.username, updateRequest.id)
+
+        const contact = await prismaClient.contact.update({
+            where: {id: updateRequest.id, username: user.username},
+            data: updateRequest
+        })
 
         return toContactResponse(contact)
     }
--- test/contact.test.ts
+++ test/contact.test.ts
@@ -65,25 +65,9 @@ describe('POST /api/contacts', () => {
         expect(response.body.errors).toBeDefined()
     })
 
-    it('should be reject if empty first_name', async () => {
-        const response = await supertest(web)
-            .post('/api/contacts')
-            .set('X-API-TOKEN', 'test')
-            .send({
-                first_name: '',
-                last_name: 'Fadel Fatonsyah',
-                email: 'aff.anton20@gmail.com',
-                phone: '1111',
-            })
-
-        logger.debug(response.body)
-        expect(response.status).toBe(400)
-        expect(response.body.errors).toBeDefined()
-    })
-
 })
 
-describe('GET /api/contacts', () => {
+describe('GET /api/contacts/:contactId', () => {
 
     beforeEach(async () => {
         await UserTest.create()
@@ -128,6 +112,74 @@ describe('GET /api/contacts', () => {
 
         logger.debug(response.body)
         expect(response.status).toBe(404)
+        expect(response.body.errors).toBeDefined()
+    })
+
+})
+
+describe('PUT /api/contacts/:contactId', () => {
+
+    beforeEach(async () => {
+        await UserTest.create()
+        await ContactTest.create()
+    })
+
+    afterEach(async () => {
+        await ContactTest.deleteAll()
+        await UserTest.delete()
+    })
+
+    it('should be update contact', async () => {
+        const contact = await ContactTest.get()
+        const response = await supertest(web)
+            .put(`/api/contacts/${contact.id}`)
+            .set('X-API-TOKEN', 'test')
+            .send({
+                first_name: 'Arrasyid',
+                last_name: 'Fadel Fatonsyah',
+                email: 'aff.anton20@gmail.com',
+                phone: '1111',
+            })
+
+        logger.debug(response.body)
+        expect(response.status).toBe(200)
+        expect(response.body.data.first_name).toBe('Arrasyid')
+        expect(response.body.data.last_name).toBe('Fadel Fatonsyah')
+        expect(response.body.data.email).toBe('aff.anton20@gmail.com')
+        expect(response.body.data.phone).toBe('1111')
+    })
+
+    it('should be reject if wrong token', async () => {
+        const contact = await ContactTest.get()
+        const response = await supertest(web)
+            .put(`/api/contacts/${contact.id}`)
+            .set('X-API-TOKEN', 'test1')
+            .send({
+                first_name: '',
+                last_name: 'Fadel Fatonsyah',
+                email: 'aff.anton20@gmail.com',
+                phone: '1111',
+            })
+
+        logger.debug(response.body)
+        expect(response.status).toBe(401)
+        expect(response.body.errors).toBeDefined()
+    })
+
+    it('should be reject if empty first_name', async () => {
+        const contact = await ContactTest.get()
+        const response = await supertest(web)
+            .put(`/api/contacts/${contact.id}`)
+            .set('X-API-TOKEN', 'test')
+            .send({
+                first_name: '',
+                last_name: 'Fadel Fatonsyah',
+                email: 'aff.anton20@gmail.com',
+                phone: '1111',
+            })
+
+        logger.debug(response.body)
+        expect(response.status).toBe(400)
         expect(response.body.errors).toBeDefined()
     })
 
